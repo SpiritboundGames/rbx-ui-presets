9f51525543521474dc406a29446bf8598dbdfa86
dev-syn <syncromaticdev@gmail.com>
2023-03-20 22:00:00 -0400

    Add Types script and Types to ItemField

---
+++ src/ItemField.lua
@@ -0,0 +1,7 @@
+---@module src/Types
+local Types = require(script.Parent.Types);
+
+local ItemField = {};
+ItemField.__index = ItemField;
+
+return ItemField;
--- src/Types.lua
+++ src/Types.lua
@@ -1,2 +1,12 @@
+-- #region ItemField
+export type Object_ItemField = {};
 
+export type Schema_ItemField = {
+    __index: Schema_ItemField,
+
+    new: () -> ()
+};
+
+export type ItemField = Object_ItemField & Schema_ItemField;
+-- #endregion
 return true;
