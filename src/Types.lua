a1cd773d2c81c05066f1878dc9a6f3bab5727b6e
dev-syn <syncromaticdev@gmail.com>
2023-03-20 22:06:29 -0400

    Add Field Enum and Field Enum Types

--- src/Types.lua
+++ src/Types.lua
@@ -1,10 +1,20 @@
+-- #region Enum_ItemFields
+
+export type Enum_ItemField = {
+    TextField: number,
+    EnumField: number
+};
+
+-- #endregion
+
 -- #region ItemField
 export type Object_ItemField = {};
 
 export type Schema_ItemField = {
     __index: Schema_ItemField,
+    Field: Enum_ItemField,
 
-    new: () -> ()
+    new: (itemField: number) -> ItemField
 };
 
 export type ItemField = Object_ItemField & Schema_ItemField;
