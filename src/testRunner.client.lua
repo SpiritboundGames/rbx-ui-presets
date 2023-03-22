2972d531726610b430e40eef67bb6fa09cdcb0c6
dev-syn <syncromaticdev@gmail.com>
2023-03-22 19:31:54 -0400

    Add ValueChanged event to test runner

--- src/testRunner.client.lua
+++ src/testRunner.client.lua
@@ -9,11 +9,14 @@ local UIPresets: Types.UIPresets = require(script.Parent);
 local PlayerGUI: PlayerGui = plr:WaitForChild("PlayerGui");
 local TestUI: ScreenGui = PlayerGUI:WaitForChild("TestUI");
 
---local testTextField: Types.TextField = UIPresets.newField(UIPresets.Field.TextField);
---testTextField:SetParent(TestUI:FindFirstChild("Frame"):FindFirstChild("ScrollingFrame"));
+local testTextField: Types.TextField = UIPresets.newField(UIPresets.Field.TextField);
+testTextField:SetParent(TestUI:FindFirstChild("Frame"):FindFirstChild("ScrollingFrame"));
+testTextField.ValueChanged:Connect(function(value: string)
+    print(`Value Changed: {value}`);
+end);
 
-local testDropdown: Types.DropdownField = UIPresets.newField(UIPresets.Field.DropdownField, {
+--[[local testDropdown: Types.DropdownField = UIPresets.newField(UIPresets.Field.DropdownField, {
     Test = "TestValue",
     AnotherTest = "AnotherTestValue"
 });
-testDropdown:SetParent(TestUI:FindFirstChild("Frame"):FindFirstChild("ScrollingFrame"));
+testDropdown:SetParent(TestUI:FindFirstChild("Frame"):FindFirstChild("ScrollingFrame"));--]]
